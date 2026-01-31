c745f4afde0944f0c90991180b9eb76bba5e9efa
kawaemon <me@kawaemon.dev>
2026-02-01 08:49:59 +0900

    bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import { analyzer } from "vite-bundle-analyzer";
+
+const plugins = [];
+
+if (process.env.ANALYZE_BUNDLE) {
+  plugins.push(analyzer({ openAnalyzer: false }));
+}
+
+plugins.push(react(), wasm(), topLevelAwait());
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins,
 });
